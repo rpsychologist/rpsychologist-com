301e564aab2b4cdec0c02d31c0cc2edb2690d5e1
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2020-09-30 15:37:45 +0000

    adds discord link

--- rpsych-cohend/src/components/content/Footer.js
+++ rpsych-cohend/src/components/content/Footer.js
@@ -7,17 +7,18 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteIcon from "@material-ui/icons/Favorite";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import HomeIcon from '@material-ui/icons/Home';
+import HomeIcon from "@material-ui/icons/Home";
+import ChatIcon from '@material-ui/icons/Chat';
 import { useStaticQuery, graphql } from "gatsby";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import logo from "../navigation/rpsychologist-logo.svg";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   footer: {
     marginTop: "auto",
@@ -25,14 +26,13 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     backgroundColor: "#2b3038",
     color: "white",
-    padding: theme.spacing(6)
+    padding: theme.spacing(6),
   },
   logoContainer: {
-
-    paddingTop: theme.spacing(3)
+    paddingTop: theme.spacing(3),
   },
   licenses: {
-    paddingTop: theme.spacing(3)
+    paddingTop: theme.spacing(3),
   },
   logo: {
     filter: "invert(100%)",
@@ -40,10 +40,10 @@ const useStyles = makeStyles(theme => ({
   },
   icon: {
     color: "rgba(255, 255, 255, 0.9)",
-  }
+  },
 }));
 
-const Footer =  React.memo(() =>{
+const Footer = React.memo(() => {
   const classes = useStyles();
   const data = useStaticQuery(
     graphql`
@@ -61,23 +61,25 @@ const Footer =  React.memo(() =>{
   return (
     <footer className={classes.footer}>
       <Container maxWidth="sm">
-        <Grid
-          container
-          className={classes.root}
-          justify="center"
-          spacing={8}
-        >
-          <Grid  sm={6} key="1" item>
-            <Grid item xs={12} >
-            <Typography variant="h6" align="left" gutterBottom>
-              About
-            </Typography>
+        <Grid container className={classes.root} justify="center" spacing={8}>
+          <Grid sm={6} key="1" item>
+            <Grid item xs={12}>
+              <Typography variant="h6" align="left" gutterBottom>
+                About
+              </Typography>
             </Grid>
             <Grid item xs={12} className={classes.logoContainer}>
-              <img src={logo} alt="RPsychologist logo" className={classes.logo} />
-            <Typography variant="body2" align="left" gutterBottom>
-              Created by <a href="https://rpsychologist.com/about">Kristoffer Magnusson</a>
-            </Typography>
+              <img
+                src={logo}
+                alt="RPsychologist logo"
+                className={classes.logo}
+              />
+              <Typography variant="body2" align="left" gutterBottom>
+                Created by{" "}
+                <a href="https://rpsychologist.com/about">
+                  Kristoffer Magnusson
+                </a>
+              </Typography>
             </Grid>
           </Grid>
           <Grid sm={6} key="2" item>
@@ -85,58 +87,89 @@ const Footer =  React.memo(() =>{
               Connect
             </Typography>
             <List component="nav" aria-label="connect">
-              <ListItem button component="a" href="https://www.twitter.com/krstoffr">
+              <ListItem
+                button
+                component="a"
+                href="https://www.twitter.com/krstoffr"
+              >
                 <ListItemIcon>
-                  <TwitterIcon className={classes.icon}/>
+                  <TwitterIcon className={classes.icon} />
                 </ListItemIcon>
                 <ListItemText primary="Twitter" />
               </ListItem>
-              <ListItem button component="a" href="https://www.github.com/rpsychologist">
+              <ListItem
+                button
+                component="a"
+                href="https://www.github.com/rpsychologist"
+              >
                 <ListItemIcon>
-                  <GitHubIcon className={classes.icon}/>
+                  <GitHubIcon className={classes.icon} />
                 </ListItemIcon>
                 <ListItemText primary="GitHub" />
               </ListItem>
-              <ListItem button component="a" href="https://www.linkedin.com/in/kristofferm">
+              <ListItem
+                button
+                component="a"
+                href="https://www.linkedin.com/in/kristofferm"
+              >
                 <ListItemIcon>
-                  <LinkedInIcon className={classes.icon}/>
+                  <LinkedInIcon className={classes.icon} />
                 </ListItemIcon>
                 <ListItemText primary="LinkedIn" />
               </ListItem>
-              <ListItem button component="a" href="https://www.rpsychologist.com">
-              <ListItemIcon>
-                  <HomeIcon className={classes.icon}/>
+              <ListItem
+                button
+                component="a"
+                href="https://www.rpsychologist.com"
+              >
+                <ListItemIcon>
+                  <HomeIcon className={classes.icon} />
                 </ListItemIcon>
                 <ListItemText primary="Blog" />
+              </ListItem>
+              <ListItem button component="a" href="https://discord.gg/8DZmg2g">
+                <ListItemIcon>
+                  <ChatIcon className={classes.icon} />
+                </ListItemIcon>
+                <ListItemText primary="Discord" />
               </ListItem>
             </List>
             <Typography variant="h6" align="left" gutterBottom>
               Donate
             </Typography>
             <List component="nav" aria-label="donate">
-              <ListItem button component="a" href="https://www.buymeacoffee.com/krstoffr">
+              <ListItem
+                button
+                component="a"
+                href="https://www.buymeacoffee.com/krstoffr"
+              >
                 <ListItemIcon>
-                  <FavoriteIcon className={classes.icon}/>
+                  <FavoriteIcon className={classes.icon} />
                 </ListItemIcon>
                 <ListItemText primary="Buy Me a Coffe" />
               </ListItem>
-              <ListItem button component="a" href="https://www.paypal.me/krstoffr">
+              <ListItem
+                button
+                component="a"
+                href="https://www.paypal.me/krstoffr"
+              >
                 <ListItemIcon>
-                  <FavoriteIcon className={classes.icon}/>
+                  <FavoriteIcon className={classes.icon} />
                 </ListItemIcon>
                 <ListItemText primary="PayPal" />
               </ListItem>
             </List>
           </Grid>
           <Grid item xs={12} className={classes.licenses}>
-        <Typography variant="h6" align="center" gutterBottom>
-          License
-        </Typography>
-        <Typography variant="subtitle1" align="center" component="p">
-          Version {data.site.siteMetadata.version}, last updated {data.site.siteMetadata.lastUpdated}. License MIT (
-          <a href={data.site.siteMetadata.github}>source code</a>).
-          Visualization is CC0.
-        </Typography>
+            <Typography variant="h6" align="center" gutterBottom>
+              License
+            </Typography>
+            <Typography variant="subtitle1" align="center" component="p">
+              Version {data.site.siteMetadata.version}, last updated{" "}
+              {data.site.siteMetadata.lastUpdated}. License MIT (
+              <a href={data.site.siteMetadata.github}>source code</a>).
+              Visualization is CC0.
+            </Typography>
           </Grid>
         </Grid>
       </Container>
