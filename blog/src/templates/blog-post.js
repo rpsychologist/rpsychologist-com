3e288c0d0c4c4d85a880319e2222431812cfb841
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2020-10-20 13:41:01 +0000

    remove comps already in pagewrapper

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -21,16 +21,7 @@ import Tags from 'gatsby-theme-rpsych/src/components/TagsPost'
 import Link from '@material-ui/core/Link'
 import InternalLink from 'gatsby-theme-rpsych/src/utils/InternalLink'
 import { withStyles } from '@material-ui/core/styles'
-import { MDXProvider } from '@mdx-js/react'
-import MuiLink from '@material-ui/core/Link'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import CodeBlock from 'gatsby-theme-rpsych/src/components/code/code-block'
 
 const StyledTableContainer = withStyles(() => ({
   root: {
@@ -38,52 +29,6 @@ const StyledTableContainer = withStyles(() => ({
     marginBottom: '2em',
   },
 }))(TableContainer)
-
-
-const components = {
-  a: props => <MuiLink {...props} />,
-  figcaption: props => <Typography {...props} />,
-  p: props => <Typography variant="body1" paragraph {...props} className="article--outer" />,
-  h1: props => <Typography variant="h2" component="h1" {...props} className="article--outer"/>,
-  h2: props => <Typography variant="h2" {...props} className="article--outer"/>,
-  h3: props => <Typography variant="h3" {...props} className="article--outer"/>,
-  h4: props => <Typography variant="h4" gutterBottom {...props} className="article--outer"/>,
-  h5: props => <Typography variant="h5" gutterBottom {...props} className="article--outer"/>,
-  h6: props => <Typography variant="h6" gutterBottom {...props} className="article--outer"/>,
-  ul: props =>  <Typography variant="body1" paragraph component="ul" {...props} className="article--outer"/>,
-  li: props =>  <Typography variant="body1" component="li" {...props} className="article--outer"/>,
-  ol: props =>  <Typography variant="body1" component="ol" {...props} className="article--outer"/>,
-  pre: ({ children }) => <CodeBlock>{children}</CodeBlock>,
-  blockquote: props => (
-    <div className="article--outer" >
-      <blockquote {...props} />
-    </div>
-  ),
-  table: props => {
-    return (
-      <StyledTableContainer component={Paper}>
-        <Table {...props} />
-      </StyledTableContainer>
-    )
-  },
-  thead: props => {
-    return <TableHead {...props} />
-  },
-  tbody: props => {
-    return <TableBody {...props} />
-  },
-  tr: props => {
-    return <TableRow {...props} />
-  },
-  td: props => {
-    return <TableCell children={props.children} align={props.align === null ? 'inherit':props.align} />
-  },
-  th: props => {
-    return <TableCell children={props.children} align={props.align === null ? 'inherit':props.align} />
-  },
-}
-
-
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -294,11 +239,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
                 />
               </Grid>
             </Grid>
-            <MDXProvider components={components}>
-              <MDXRenderer>{post.body}</MDXRenderer>
-            </MDXProvider>
-
-
+            <MDXRenderer>{post.body}</MDXRenderer>
             <div className={classes.articleInner}>
               <Divider style={{ marginBottom: '1em', marginTop: '1em' }} />
               <Bio />
