8b382567e9a51237a086b91a1edeac02d1f39cf8
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2020-10-20 13:44:55 +0000

    remove debug

--- gatsby-theme-rpsych/src/components/start/Hero.js
+++ gatsby-theme-rpsych/src/components/start/Hero.js
@@ -28,7 +28,6 @@ const useStyles = makeStyles(theme => ({
 const Hero = props => {
   const { location, title, children } = props
   const data = useStaticQuery(bioQuery)
-  console.log(data)
   const { social } = data.site.siteMetadata
 
   const classes = useStyles()
