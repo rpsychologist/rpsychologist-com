19e73f20709fed8751a38afdb4f76b22009164c5
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2020-11-16 07:48:08 +0000

    fix setting inputs

--- gatsby-theme-rpsych-viz/src/components/SettingsInput.js
+++ gatsby-theme-rpsych-viz/src/components/SettingsInput.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
+import CloseIcon from '@material-ui/icons/Close';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx'
 
 const useStyles = makeStyles(() => ({
   textField: {
-    minWidth: "100%"
+    minWidth: "100%",
+    padding: 0,
+    '& .MuiFilledInput-adornedEnd': {
+      paddingRight: 0,
+    }
+  },
+  unsubmitted: {
+    backgroundColor: "rgb(0, 102, 255, 0.09)"
+  },
+
+  clear: {
+    '& .MuiIconButton-root': {
+      padding: '10px'
+    }
   }
+
 }));
 
 const SettingsInput = ({
@@ -23,6 +41,7 @@ const SettingsInput = ({
   const classes = useStyles();
   const inputProps = { min: min, max: max, step: step };
   const [error, setError] = useState(false);
+  const [submitted, setSubmitted] = useState(true);
   const [input, setInput] = useState(value);
 
   useEffect(() => {
@@ -30,18 +49,32 @@ const SettingsInput = ({
   }, [value]);
 
   const onChange = e => {
-    const value = e.target.type === "number" ? Number(e.target.value) : e.target.value;
+    const value = e.target.type === "number" ? e.target.value : e.target.value;
     const checkInput = (name === "M0") | (name === "M1") | name === "CER" | name === "SD";
     if (!((value > max) & checkInput) & !((value < min) & checkInput)) {
-      handleChange({
-        name: e.target.name,
-        value: value
-      });
+
       setError(false);
     } else {
       setError(true);
     }
+    setSubmitted(false)
     setInput(value)
+  };
+  const onSubmit = (e) => {
+    switch (e.key) {
+      case "Enter":
+        setSubmitted(true)
+        handleChange({
+          name: e.target.name,
+          value: input,
+        });
+        break;
+      case "Esc":
+      case "Escape":
+        setSubmitted(true)
+        setError(false)
+        break;
+    }
   };
   return (
 
@@ -52,15 +85,33 @@ const SettingsInput = ({
           label={label}
           name={name}
           type={type}
-          className={classes.textField}
+          className={clsx({
+            [classes.textField]: true,
+            [classes.unsubmitted]: !submitted
+
+          })
+           }
           InputLabelProps={{
             shrink: true
           }}
-          inputProps={inputProps}
+          InputProps={{ 
+            ...inputProps,
+            endAdornment: (
+              <InputAdornment position="end" className={classes.clear}>
+                <IconButton
+                  disabled={submitted}
+                  aria-label="toggle password visibility"
+                  onClick={() => setSubmitted(true)}
+                >
+                  < CloseIcon fontSize="small" />
+                </IconButton>
+              </InputAdornment>
+             ) }}
           margin="dense"
-          variant="filled"
+          variant={"filled"}
           onChange={onChange}
-          value={input}
+          onKeyDown={onSubmit}
+          value={submitted ? value : input}
           helperText={helperText}
         />
   );
