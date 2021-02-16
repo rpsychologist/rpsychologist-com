c89fcad6d4ed254825aa055fcde1d7bf1d5b51aa
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2021-02-16 14:54:11 +0100

    fix webmentions on translated pages

--- gatsby-theme-rpsych/src/utils/helpers.js
+++ gatsby-theme-rpsych/src/utils/helpers.js
@@ -1,6 +1,6 @@
 const createURLRegEx = (slug, d3Slug=false) => {
     const isRoot = slug == "/"
-    const cleanedSlug = isRoot ? '' : slug.replace(/\//g, '') + '\/?'
+    const cleanedSlug = isRoot ? '' : slug.replace(/^\/|\/$/g, '') + '\/?'
     const regex = d3Slug
       ? `^https?:\/\/rpsychologist\.com\/?(d3\/)?${cleanedSlug}(index\.html)?\/?$/`
       : `^https?:\/\/rpsychologist\.com\/?${cleanedSlug}(index\.html)?\/?$/`
